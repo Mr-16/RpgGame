c510b01c7ecbeee5c263717a9479005887defcf4
stj <2398827914@qq.com>
2025-11-27 21:24:06 +0800

    搭建攻击状态机框架, 分为空闲, 普攻, 技能, 冷却四个状态

---
+++ Immortal/Scripts/Characters/Players/StateMachines/AttackStateMachine.cs
@@ -0,0 +1,34 @@
+using RpgGame.Scripts.Characters.Players.States.MovementStates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgGame.Scripts.Characters.Players.StateMachines
+{
+    public class AttackStateMachine
+    {
+        public StateBase curState;
+        //public IdleState idleState;
+        //public WalkState walkState;
+        //public RunState runState;
+        //public RollState rollState;
+
+        public AttackStateMachine(Player player)
+        {
+            //idleState = new IdleState(player);
+            //walkState = new WalkState(player);
+            //runState = new RunState(player);
+            //rollState = new RollState(player);
+            //ChangeState(idleState);
+        }
+        
+        public void ChangeState(StateBase newState)
+        {
+            if (curState != null) curState.Exit();
+            curState = newState;
+            curState.Enter();
+        }
+    }
+}
---
+++ Immortal/Scripts/Characters/Players/StateMachines/AttackStates/CooldownState.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgGame.Scripts.Characters.Players.StateMachines.AttackStates
+{
+    public class CooldownState : StateBase
+    {
+        public CooldownState(Player player)
+        {
+            this.player = player;
+        }
+
+        public override void Enter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(float delta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void FixedUpdate(float delta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Exit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
